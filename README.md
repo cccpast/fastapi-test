04d27d6604c0ca33a80af003642544fe39461d55
cccpast <coltpex13fix@gmail.com>
2024-02-14 19:51:05 +0900

    study req param

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # fastapi-test
+
+## Run
+
+```bash
+uvicorn main:app --reload
+```
+
+## Access
+
+```
+http://127.0.0.1:8000
+```
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from typing import Union
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 def read_root():
   return {"message": "Hello, fastapi!!"}
+
+# http://127.0.0.1:8000/items/2?q=hogehoge
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+  return {"item_id": item_id, "q": q}
